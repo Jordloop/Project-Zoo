707486aecc0e92bd06d81080b31404e704761847
JordLoop <JordLoop@gmail.com>
2017-07-07 11:27:47 -0700

    add EventEmitter() to send an action to component.ts

--- app/list-animal.component.ts
+++ app/list-animal.component.ts
@@ -12,16 +12,22 @@ import { Animal } from './animal.model';
   </select>
   <ul>
   <li *ngFor="let currentAnimal of childAnimalList | age:filterByAge">{{currentAnimal.name}}, {{currentAnimal.species}}, {{currentAnimal.age}}, {{currentAnimal.diet}}, {{currentAnimal.location}}, {{currentAnimal.caretakers}}, {{currentAnimal.sex}}, {{currentAnimal.like}}, {{currentAnimal.dislike}}</li>
+  <button (click)="editButtonClicked(currentAnimal)">Edit</button>
   </ul>
   `
 })
 
 export class ListAnimalComponent {
   @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
 
-filterByAge: string = "all";
+  filterByAge: string = "all";
 
   filterList(menuOption) {
     this.filterByAge = menuOption;
+  }
+
+  editButtonClicked(animalToEdit: Animal) {
+    this.clickSender.emit(animalToEdit);
   }
 }
