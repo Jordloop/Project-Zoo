b1a0ac43d97e23096edf42bc7104330d1792f141
JordLoop <JordLoop@gmail.com>
2017-07-07 12:16:33 -0700

    add complete edit-animal.component functionality

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -6,8 +6,19 @@ import { Animal } from './animal.model';
   template: `
   <div *ngIf="childSelectedAnimal" class="Well">
     <h1>EDIT</h1>
-    <label>Enter Animal Name:</label>
+
+    <label>Enter Animal's Name:</label>
     <input [(ngModel)]=childSelectedAnimal.name>
+    <label>Enter Animal's Age:</label>
+    <input [(ngModel)]=childSelectedAnimal.age>
+    <label>Enter Animal's Location:</label>
+    <input [(ngModel)]=childSelectedAnimal.location>
+    <label>Enter Animal's Caretakers:</label>
+    <input [(ngModel)]=childSelectedAnimal.caretakers>
+    <label>Enter Animal's Likes:</label>
+    <input [(ngModel)]=childSelectedAnimal.like>
+    <label>Enter Animal's Dislikes:</label>
+    <input [(ngModel)]=childSelectedAnimal.dislike>
     <button (click)="doneButtonClicked()">Done</button>
   </div>
 
