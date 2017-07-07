e4d56f4046204c3b68076d81d701489c4784b269
JordLoop <JordLoop@gmail.com>
2017-07-07 10:40:10 -0700

    add ability to filter animals on task-list.component

--- app/list-animal.component.ts
+++ app/list-animal.component.ts
@@ -5,6 +5,11 @@ import { Animal } from './animal.model';
 ({
   selector: 'list-animal',
   template: `
+  <select (change)="filterList$event.target.value)">
+    <option value="all">All</option>
+    <option value="young">Young</option>
+    <option value="mature">Mature<</option>
+  </select>
   <ul>
   <li *ngFor="let currentAnimal of childAnimalList">{{currentAnimal.name}}, {{currentAnimal.species}}, {{currentAnimal.age}}, {{currentAnimal.diet}}, {{currentAnimal.location}}, {{currentAnimal.caretakers}}, {{currentAnimal.sex}}, {{currentAnimal.like}}, {{currentAnimal.dislike}}</li>
   </ul>
@@ -13,4 +18,10 @@ import { Animal } from './animal.model';
 
 export class ListAnimalComponent {
   @Input() childAnimalList: Animal[];
+
+filterByAge: string = "all";
+
+  filterList(menuOption) {
+    this.filterByAge = menuOption;
+  }
 }
