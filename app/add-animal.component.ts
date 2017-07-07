51a5f5a5eae8d6dc778068c8828fbae30331aae9
JordLoop <JordLoop@gmail.com>
2017-07-07 12:27:04 -0700

    add imports, component and export to add-animal.component

---
+++ app/add-animal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component
+({
+  selector: 'add-animal',
+  template: `
+
+
+  `
+})
+
+export class addAnimalComponent {
+
+
+}
